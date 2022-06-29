f297b9c07e3e873edd72565580cb6df74141778f
rukavinaet <git@rukavinaet.com>
2022-06-29 18:08:39 +0200

    start_meta

--- article.php
+++ article.php
@@ -11,34 +11,29 @@ else{
     header("Location: /");
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en" class="no-js">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
-    <title><?php echo $row['articleName'] ?></title> <!-- TODO: update page title -->
-    <script type="module">
-        document.documentElement.classList.remove('no-js');
-        document.documentElement.classList.add('js');
-    </script>
-    <meta name="description" content="Page Description"> <!-- TODO: update meta description -->
-    <meta property="og:title" content="Home - Page"> <!-- TODO: update og:title -->
-    <meta property="og:url" content="https://www.example.com/page"> <!-- TODO: update og:url -->
-    <meta property="og:description" content="OG Page Description"> <!-- TODO: update og:description -->
-    <meta property="og:image" content="/path/to/image"> <!-- TODO: og:image -->
-    <meta property="og:image:alt" content="OG Image Description"> <!-- TODO: update og:image:alt -->
+    <title><?php echo $row['articleName'] ?></title> 
+    <meta name="description" content="<?php echo $row['ArticleMetaDesc'] ?>"> 
+    <meta property="og:title" content="Home - Page"> 
+    <meta property="og:url" content="https://www.example.com/page"> 
+    <meta property="og:description" content="OG Page Description"> 
+    <meta property="og:image" content="/path/to/image">
+    <meta property="og:image:alt" content="OG Image Description">
     <meta property="og:locale" content="en_US">
     <meta property="og:type" content="website">
-    <meta name="twitter:card" content="large-image-twitter.jpg"> <!-- TODO: update twitter:cared -->
-    <link rel="canonical" href="https://www.example.com/page"> <!-- TODO: update canonical link -->
+    <meta name="twitter:card" content="large-image-twitter.jpg"> 
+    <link rel="canonical" href="https://www.example.com/page"> 
     <link rel="icon" href="/favicon.ico">
     <link rel="icon" href="/favicon.svg" type="image/svg+xml">
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-    <link rel="manifest" href="site.webmanifest"> <!-- TODO: update web app manifest file -->
-    <meta name="theme-color" content="#FF00FF"> <!-- TODO: update meta theme color -->
-    <link rel="stylesheet" href="/styles/css/styles.css"> <!-- TODO: Update styles -->
+    <link rel="manifest" href="site.webmanifest"> 
+    <meta name="theme-color" content="#FF00FF"> >
+    <link rel="stylesheet" href="/styles/css/styles.css"> 
     <link rel="stylesheet" href="/styles/css/print.css" media="print">
 </head>
 

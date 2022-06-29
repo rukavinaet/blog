361d9b973d2d3b5e65cca23efec16ed9b427ecec
rukavinaet <git@rukavinaet.com>
2022-06-29 16:43:16 +0200

    a

--- article.php
+++ article.php
@@ -1,13 +1,10 @@
 <?php
 if(isset($_GET['ID'])){
     require_once 'con.php';
-
     $ID = mysqli_real_escape_string($conn, $_GET['ID']);
-
     $sql = "SELECT * FROM `blog_article` WHERE articleID = '$ID' ";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($result);
-
     if (mysqli_num_rows($result)==0) { header("Location: /"); }
 }
 else{
